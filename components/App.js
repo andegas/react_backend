c642b37b1e1beff032197f1e667b5a7e79a06085
Hayk.Grigoryan <hayk.grigoryan@helpsystems.com>
2019-03-28 19:29:56 +0400

    react fronf code

---
+++ components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App export React.PureComponent {
+
+    render(){
+        return (
+            <div>
+                App Component
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+
