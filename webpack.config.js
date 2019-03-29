6ac7c602b00e74a8e5d212f594ff929588425783
Hayk.Grigoryan <hayk.grigoryan@helpsystems.com>
2019-03-29 19:52:26 +0400

    new webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const webpack = require('webpack');
+const config = {
+    resolve: {
+        modules: [path.resolve('./lib'), path.resolve('./node_modules')]
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].js'
+    },
+}
+
+module.exports = config;
