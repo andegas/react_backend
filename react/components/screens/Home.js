a29e9bde9b47914c5ec2fe87b3cac6b8704fa3fa
mk <mk@mail.ru>
2019-04-20 18:22:11 +0400

    Merge branch 'form-builder' of D:\react_backend with conflicts.

--- react/components/screens/Home.js
+++ react/components/screens/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {setDataStore} from '../../actions';
 import List from '../List';
 import Form from '../Form';
 import Result from '../Result';
 
+
 class Home extends React.Component{
     render(){
+        console.log(this.props);
         return (
             <div className="container-fluid">
                 <p>Home screen</p>
@@ -24,4 +28,12 @@ class Home extends React.Component{
     }
 }
 
-export default Home;
+const mapStateToProps = state =>{
+
+    return {
+        data: state.data.data
+    };
+
+};
+
+export default connect(mapStateToProps,{setDataStore})(Home);
