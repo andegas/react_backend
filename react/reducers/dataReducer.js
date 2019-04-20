ec8f67cd1cd72c096b2a5b2c9d7c5a14abd0764a
test <test@mail.ru>
2019-04-20 19:32:49 +0400

    reducers

--- react/reducers/dataReducer.js
+++ react/reducers/dataReducer.js
@@ -1,16 +1,23 @@
 import Actions from '../actionsTypes';
 
-const INITIAL_STATE = {
+const initState = {
     data: []
 };
 
-export default (state = INITIAL_STATE, action) => {
+export default  (state = initState, action) => {
     switch (action.type) {
-    case Actions.SET_DATA_IN_STORE:
-        return {
-            ...state,
-            data: action.payload
-        };
+    case Actions.GET_VIEW:
+        return {...state = initState};
+    case Actions.INPUT_VIEW_RECEIVED:
+        return { ...state, inpView: action.jsonInp};
+    case Actions.GET_VIEW_TEXTAREA:
+        return {...state = initState};
+    case Actions.TEXTAREA_VIEW_RECEIVED:
+        return { ...state, txtAreaView: action.jsonTxtArea};
+    case Actions.GET_VIEW_SELECT_OPTION:
+        return {...state = initState};
+    case Actions.SELECT_OPTION_VIEW_RECEIVED:
+        return { ...state, selectOptView: action.jsonSelectOpt};
     default:
         return state;
     }
--- react/reducers/index.js
+++ react/reducers/index.js
@@ -1,31 +1,6 @@
-// import {combineReducers} from 'redux';
-// import dataReducer from './dataReducer';
+import {combineReducers} from 'redux';
+import dataReducer from './dataReducer';
 
-// export default combineReducers({
-//     data: dataReducer
-// });
-
-const initState = {
-    data: []
-};
-
-const reducer = (state = initState, action) => {
-    switch (action.type) {
-    case 'GET_VIEW':
-        return {...state = initState};
-    case 'INPUT_VIEW_RECEIVED':
-        return { ...state, inpView: action.jsonInp};
-    case 'GET_VIEW_TEXTAREA':
-        return {...state = initState};
-    case 'TEXTAREA_VIEW_RECEIVED':
-        return { ...state, txtAreaView: action.jsonTxtArea};
-    case 'GET_VIEW_SELECT_OPTION':
-        return {...state = initState};
-    case 'SELECT_OPTION_VIEW_RECEIVED':
-        return { ...state, selectOptView: action.jsonSelectOpt};
-    default:
-        return state;
-    }
-};
-
-export default reducer;
+export default combineReducers({
+    data: dataReducer
+});
